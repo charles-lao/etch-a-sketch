16136dcbd5e4242eea75af69fd15fd1dbd9dccc5
charles-lao <charles.h.lao@gmail.com>
2022-10-27 00:21:58 +0800

    Create basic 16x16 grid

---
+++ script.js
@@ -0,0 +1,34 @@
+const container = document.querySelector('#container');
+let rows = document.getElementsByClassName('rows');
+let cells = document.getElementsByClassName('cells');
+
+let rowCount = 16;
+let columnCount = 16; 
+
+
+function createGrid(){
+    createRows(rowCount);
+    createColumns(columnCount);
+}
+
+function createRows(rowNum){
+    for (let r = 0; r < rowNum; r++) {
+        let row = document.createElement('div');
+        container.appendChild(row);
+        row.setAttribute('class', 'rows');
+    }
+}
+
+function createColumns(columnNum) {
+    for (let r = 0; r < rowCount; r++) {
+        for (let c = 0; c<columnNum; c++) {
+            let cell = document.createElement('div');
+            cell.setAttribute('class', 'cells');
+            rows[r].appendChild(cell);
+            console.log(r+' '+c);
+        }
+    }
+}
+
+
+createGrid();
